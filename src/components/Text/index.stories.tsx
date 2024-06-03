9a7364719f162a15bc83428bb995c03caa5545dd
alenavarroxp <alejandropvp13@gmail.com>
2024-06-03 20:00:52 +0200

    feat: Add Text component with styling options
    - Create Text component with support for various styling options such as emphasis, size, weight, alignment, italic, and underline
    - Implement default styling options for size and alignment
    - Add support for customizing the component's HTML tag using the 'as' prop
    - Update Button component to use forwardRef for better performance
    
    Refactor folder

---
+++ src/components/Text/index.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Text } from ".";
+
+const meta: Meta<typeof Text> = {
+  title: "Components/Text",
+  component: Text,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    as: "h1",
+    children: "be parts correct potatoes sides donkey extra climate",
+  },
+};
